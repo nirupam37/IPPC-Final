17693286e35f8fd8d6584334fd5d795594ec341e
Avishek Golder <59996779+AvishekGolder@users.noreply.github.com>
2021-01-19 21:00:35 +0600

    Update b.cpp

--- b.cpp
+++ b.cpp
@@ -1,14 +1,28 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <algorithm>
+#define endl '\n'
+#define pi acos(-1)
+#define pii pair<int,int>
+#define pb push_back
+#define all(a) a.begin(),a.end()
 #define ll long long
+#define ull unsigned long long
+#define io freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define boost ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+void mycode();
+//__int128
 int main(){
+  //boost;
+  //io;
+  mycode();
+  return 0;
+}
+void mycode(){
 	int a,b;
 	cin>>a>>b;
 	printf("%d\n",a+b);
-	return 0;
 }
 //Avishek Golder
 //Khulna Polytechnic Institute
 //Good Bye
-
-
